ade82ee51044a8627760270f42b6696f769f9d10
qiufengshe <sunqiusheng827@126.com>
2021-08-31 22:37:19 +0800

    1. ae通过宏屏蔽系统的差异

--- src/config.h
+++ src/config.h
@@ -1,10 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+//根据编译器内置的针对系统的宏去定义宏
+//1.如果是FreeBSD和Mac系统就动议HAVE_KQUEUE
 #if defined(__FreeBSD__) || defined(__APPLE__)
 #define HAVE_KQUEUE
+//2. 如果是Linux 就定义HAVE_EPOlL
 #elif defined(__linux__)
 #define HAVE_EPOLL
+//3. 如果是Solaris,就定义HAVE_EVPORT,还引入对应的头文件
 #elif defined (__sun)
 #define HAVE_EVPORT
 #define _XPG6
